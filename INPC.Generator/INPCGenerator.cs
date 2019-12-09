506af0ce3198aa8fef95a463138723636359ef32
Jérôme Laban <jerome.laban@nventive.com>
2019-12-08 21:17:16 -0500

    Add project files.

---
+++ INPC.Generator/INPCGenerator.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Linq;
+using Uno.RoslynHelpers;
+using Uno.SourceGeneration;
+
+namespace INPC.Generator
+{
+    public class INPCGenerator : SourceGenerator
+    {
+        public override void Execute(SourceGeneratorContext context)
+        {
+            // Search for the GeneratedPropertyAttribute symbol
+            var _generatedPropertyAttributeSymbol =
+                context.Compilation.GetTypeByMetadataName("INPC.GeneratedPropertyAttribute");
+
+            if(_generatedPropertyAttributeSymbol != null)
+            {
+// Search in all types defined in the current compilation (not in the dependents)
+var query = from typeSymbol in context.Compilation.SourceModule.GlobalNamespace.GetNamespaceTypes()
+            from property in typeSymbol.GetFields()
+                            
+            // Find the attribute on the field
+            let info = property.FindAttributeFlattened(_generatedPropertyAttributeSymbol)
+            where info != null
+
+            // Group properties by type
+            group property by typeSymbol into g
+            select g;
+
+foreach(var type in query)
+{
+  // Let's generate the needed class
+  var builder = new IndentedStringBuilder();
+
+  builder.AppendLineInvariant("using System;");
+  builder.AppendLineInvariant("using System.ComponentModel;");
+
+  using (builder.BlockInvariant($"namespace {type.Key.ContainingNamespace}"))
+  {
+    using(builder.BlockInvariant($"partial class {type.Key.Name} : INotifyPropertyChanged"))
+    {
+      builder.AppendLineInvariant($"public event PropertyChangedEventHandler PropertyChanged;");
+
+      foreach(var fieldInfo in type)
+      {
+        var propertyName = fieldInfo.Name.TrimStart('_');
+
+        // Uppercase name for camel case
+        propertyName = propertyName[0].ToString().ToUpperInvariant() + propertyName.Substring(1);
+
+        using (builder.BlockInvariant($"public {fieldInfo.Type} {propertyName}"))
+        {
+          builder.AppendLineInvariant($"get => {fieldInfo.Name};");
+
+          using (builder.BlockInvariant($"set"))
+          {
+            builder.AppendLineInvariant($"var previous = {fieldInfo.Name};");
+            builder.AppendLineInvariant($"{fieldInfo.Name} = value;");
+            builder.AppendLineInvariant($"PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof({propertyName})));");
+            builder.AppendLineInvariant($"On{propertyName}Changed(previous, value);");
+          }
+        }
+
+        builder.AppendLineInvariant($"partial void On{propertyName}Changed({fieldInfo.Type} previous, {fieldInfo.Type} value);");
+      }
+    }
+  }
+
+  var sanitizedName = type.Key.ToDisplayString().Replace(".", "_").Replace("+", "_");
+  context.AddCompilationUnit(sanitizedName, builder.ToString());
+}
+            }
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using INPC;
+using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new MyClass();
+            c.IntProperty = 42;
+            c.StringProperty = "My 42";
+        }
+    }
+
+    public partial class MyClass
+    {
+        [GeneratedProperty]
+        private string _stringProperty;
+
+        [GeneratedProperty]
+        private int _intProperty;
+
+        private bool _otherField;
+
+        partial void OnIntPropertyChanged(int previous, int value)
+            => Console.WriteLine($"OnIntPropertyChanged({previous},{value})");
+
+        partial void OnStringPropertyChanged(string previous, string value) 
+            => Console.WriteLine($"OnIntPropertyChanged({previous},{value})");
+    }
+
+}
+
+namespace INPC
+{
+    public class GeneratedPropertyAttribute : Attribute { }
+}
